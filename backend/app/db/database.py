501ddc219795649bdf185bc048165af56fc2e7ba
Awaish-prog <awaishk640@gmail.com>
2026-04-13 09:11:11 +0000

    updated card schemas

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -4,7 +4,8 @@ from typing import Annotated
 from fastapi import Depends
 import os
 
-DATABASE_URL = "postgresql+asyncpg://user:password@db:5432/app_db"
+# DATABASE_URL = "postgresql+asyncpg://user:password@db:5432/app_db"
+DATABASE_URL = "postgresql+asyncpg://user:password@localhost:5432/app_db"
 
 engine = create_async_engine(
     DATABASE_URL,
--- backend/app/schemas/cards.py
+++ backend/app/schemas/cards.py
@@ -1,24 +1,31 @@
 from pydantic import BaseModel, ConfigDict, Field
-from typing import Optional
+from typing import Optional, Annotated
+
+name_type = Annotated[str, Field(min_length=3, max_length=30)]
+description_type = Annotated[str, Field(min_length=3, max_length=30)]
 
 class CardBase(BaseModel):
     id: int
     
+class CardCreate(BaseModel):
+    card_name: name_type
+    description: description_type
+    card_list_id: int
+    order_key: Optional[str] = None
+    
 class CardUpdate(BaseModel):
-    card_name: str = Field(..., max_length=15, min_length=3)
-    description: str = Field(..., max_length=50, min_length=3)
+    card_name: name_type
+    description: description_type
     
 class CardOrderUpdate(BaseModel):
+    card_list_id: int
     prev_card_id: Optional[int] = None
     next_card_id: Optional[int] = None
-    card_list_id: int = None
-    
-class CardCreateRequest(CardUpdate):
-    card_list_id: int
-    
-class CardCreate(CardCreateRequest):
-    order_key: str = None
 
-class Card(CardBase, CardCreate):
+class Card(CardBase):
+    card_name: name_type
+    description: description_type
+    card_list_id: int
+    order_key: str
     
     model_config = ConfigDict(from_attributes=True)
